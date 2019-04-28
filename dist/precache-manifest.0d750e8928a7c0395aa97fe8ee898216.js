e16e46f09d303ff6d70d8432f2da22f0d7807e71
wien Wu <wien.wuwei@gmail.com>
2019-04-28 09:54:22 +0800

    build

---
+++ dist/precache-manifest.0d750e8928a7c0395aa97fe8ee898216.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = [
+  {
+    "revision": "e14432c43986c8c07792",
+    "url": "/p___Dashboard__software__model.js.async.js"
+  },
+  {
+    "revision": "300c9c5eb8586f81ea67",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "bb0cf0514b1c937e5c8c",
+    "url": "/viz.async.js"
+  },
+  {
+    "revision": "d90154882b48ecc89136",
+    "url": "/p___Dashboard__software__software.async.js"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "729bc9bcc83ae5fadccfb3cdacd81e11",
+    "url": "/static/njit.729bc9bc.png"
+  },
+  {
+    "revision": "40d806ce42c4aa6ae83b",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "40d806ce42c4aa6ae83b",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "300c9c5eb8586f81ea67",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "7d32446f485e1c32c8c9",
+    "url": "/p___Individual__Settings__model.js.async.js"
+  },
+  {
+    "revision": "f7e30f2851431bdc3291",
+    "url": "/p__User__Register.chunk.css"
+  },
+  {
+    "revision": "71019e0d3809c21aebc9",
+    "url": "/p__User__RegisterResult.chunk.css"
+  },
+  {
+    "revision": "71019e0d3809c21aebc9",
+    "url": "/p__User__RegisterResult.async.js"
+  },
+  {
+    "revision": "d77e87d98ae2fe903177",
+    "url": "/p__User__models__register.js.async.js"
+  },
+  {
+    "revision": "94510b52d3214c05861f",
+    "url": "/p___Account__student__model.js.async.js"
+  },
+  {
+    "revision": "54132e0971d03d4f0103",
+    "url": "/p___Account__student__student.chunk.css"
+  },
+  {
+    "revision": "54132e0971d03d4f0103",
+    "url": "/p___Account__student__student.async.js"
+  },
+  {
+    "revision": "01c8ff7550b29976a50b",
+    "url": "/p___Account__system__model.js.async.js"
+  },
+  {
+    "revision": "4e7335222238335c47a3",
+    "url": "/p___Account__system__system.chunk.css"
+  },
+  {
+    "revision": "4e7335222238335c47a3",
+    "url": "/p___Account__system__system.async.js"
+  },
+  {
+    "revision": "7ab4d5b37caeed8ad408",
+    "url": "/p___Dashboard__cpu__cpu.chunk.css"
+  },
+  {
+    "revision": "7ab4d5b37caeed8ad408",
+    "url": "/p___Dashboard__cpu__cpu.async.js"
+  },
+  {
+    "revision": "b7b6350e42e921ab757f",
+    "url": "/p___Dashboard__cpu__model.js.async.js"
+  },
+  {
+    "revision": "71479906cd6e16bb8ccb",
+    "url": "/p___Dashboard__equipment__equipment.chunk.css"
+  },
+  {
+    "revision": "71479906cd6e16bb8ccb",
+    "url": "/p___Dashboard__equipment__equipment.async.js"
+  },
+  {
+    "revision": "8e766fe13fa40ac38816",
+    "url": "/p___Dashboard__equipment__model.js.async.js"
+  },
+  {
+    "revision": "9d0ca12a413df7809cd1",
+    "url": "/p___Individual__Settings__Info.async.js"
+  },
+  {
+    "revision": "d90154882b48ecc89136",
+    "url": "/p___Dashboard__software__software.chunk.css"
+  },
+  {
+    "revision": "9d0ca12a413df7809cd1",
+    "url": "/p___Individual__Settings__Info.chunk.css"
+  },
+  {
+    "revision": "da6cdb8a317f5384fa159cde1f33b0f0",
+    "url": "/ggeditor/flow/start.svg"
+  },
+  {
+    "revision": "24a4ddea1095c80230d8",
+    "url": "/layouts__UserLayout.chunk.css"
+  },
+  {
+    "revision": "f7e30f2851431bdc3291",
+    "url": "/p__User__Register.async.js"
+  },
+  {
+    "revision": "d4ceff111f4101fd89c4",
+    "url": "/p__User__Login.chunk.css"
+  },
+  {
+    "revision": "424df74ad6d2fff5dd59",
+    "url": "/p__404.async.js"
+  },
+  {
+    "revision": "0e530af6fe2f20b65c32fcee3b608160",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d4ceff111f4101fd89c4",
+    "url": "/p__User__Login.async.js"
+  },
+  {
+    "revision": "bb1fde8c0c658093c0c283f313069467",
+    "url": "/ggeditor/koni/bank.svg"
+  },
+  {
+    "revision": "ef855a60cca1cecf2d56",
+    "url": "/layouts__BasicLayout.async.js"
+  },
+  {
+    "revision": "ef855a60cca1cecf2d56",
+    "url": "/layouts__BasicLayout.chunk.css"
+  },
+  {
+    "revision": "24a4ddea1095c80230d8",
+    "url": "/layouts__UserLayout.async.js"
+  },
+  {
+    "revision": "f161daaf3b30acfccfae4f38099c2161",
+    "url": "/ggeditor/koni/country.svg"
+  },
+  {
+    "revision": "7db0da6a572c4d946d955e42d8268844",
+    "url": "/ggeditor/flow/normal.svg"
+  },
+  {
+    "revision": "ee97857a1c4c0203bba4fc53018b7c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191d7c7262c8bf540dd6e4be8f51f0eb",
+    "url": "/ggeditor/koni/icon.svg"
+  },
+  {
+    "revision": "0036cca03a82d851933f5e4b5789caaa",
+    "url": "/ggeditor/koni/person.svg"
+  },
+  {
+    "revision": "347329d0982682b42d40c8162817a940",
+    "url": "/icons/icon-128x128.png"
+  },
+  {
+    "revision": "5661ce8e2472d2727e8fd0390b22ba37",
+    "url": "/icons/icon-512x512.png"
+  },
+  {
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "/icons/icon-192x192.png"
+  },
+  {
+    "revision": "7e8663d516fab38eff38b94b08da5d76",
+    "url": "/ggeditor/flow/model.svg"
+  },
+  {
+    "revision": "5b76a22dfbcc5c776e99cb817c3d36ea",
+    "url": "/ggeditor/flow/decision.svg"
+  },
+  {
+    "revision": "729bc9bcc83ae5fadccfb3cdacd81e11",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "74d3cde1ae531fef88eb",
+    "url": "/22.chunk.css"
+  },
+  {
+    "revision": "74d3cde1ae531fef88eb",
+    "url": "/22.async.js"
+  }
+];
